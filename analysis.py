cb262f775da6dc98b9b881a60b158c35da0b67a3
Jack <jacksambournweeks@gmail.com>
2021-07-03 21:45:00 +0100

    First commit cus I'm a dumdum

---
+++ analysis.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import json
+import yfinance as yf
+
+with open('output.json', "r") as file:
+    data = json.load(file)
+for token in data.keys():
+    USD_GBP = yf.download('GBPUSD=X', period='1d', interval='1h')
+    exchange = USD_GBP['Close'][-1]
+    df = pd.read_csv(token + '_data.csv')
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['% Change in Balance'] = (df.Balance.pct_change(periods=12)) * 100
+    df['Balance Diff'] = df.Balance.diff()
+    df['Value $ '] = df['Balance'] * data[token]['Current Price']
+    df['Value £ '] = df['Value $ '] * exchange
+    df['Change in Value'] = df['Balance Diff'] * df['Price']
+
+
+    x = df.groupby(pd.Grouper(key = 'Date', freq='1h'))['Balance Diff'].mean()
+    y = df.groupby(pd.Grouper(key = 'Date', freq='1D'))['Balance Diff'].mean()
+    z = df.groupby(pd.Grouper(key = 'Date', freq='1W'))['Balance Diff'].mean()
+    p = df.groupby(pd.Grouper(key = 'Date', freq='1M'))['Balance Diff'].mean()
+
+    average_hourly_difference = x.mean()
+    average_hourly_dollar_gainz = round(average_hourly_difference * data[token]['Current Price'], 2)
+
+    average_daily_difference = y.mean()
+    average_daily_dollar_gainz = round(average_daily_difference * data[token]['Current Price'], 2)
+
+    average_weekly_difference = z.mean()
+    average_weekly_dollar_gainz = round(average_weekly_difference * data[token]['Current Price'], 2)
+
+    average_monthly_difference = p.mean()
+    average_monthly_dollar_gainz = round(average_monthly_difference * data[token]['Current Price'], 2)
+
+    data[token]['Average_Hourly_Increase'] = average_hourly_difference
+    data[token]['Average_Hourly_Value_Increase'] = average_hourly_dollar_gainz
+
+    data[token]['Average_Daily_Increase'] = average_daily_difference
+    data[token]['Average_Daily_Value_Increase'] = average_daily_dollar_gainz
+
+    data[token]['Average_Weekly_Increase'] = average_weekly_difference
+    data[token]['Average_Weekly_Value_Increase'] = average_weekly_dollar_gainz
+
+    data[token]['Average_Monthly_Increase'] = average_monthly_difference
+    data[token]['Average_Monthly_Value_Increase'] = average_monthly_dollar_gainz
+
+    df.to_excel('Excels/'+ token + '_analysis.xls', index=False)
+
+
+
+
+with open("output.json", "w") as outfile:
+    json.dump(data, outfile, indent=4)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,179 @@
+import time
+import os
+import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import coinmarketcapapi
+import pandas as pd
+import json
+from pyvirtualdisplay import Display
+
+
+options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+# options.add_argument("--window-size=1920,1080")
+options.add_argument('--ignore-certificate-errors')
+storage = {
+    'XCH': {
+        'Current Price': float(),
+        'Current Balance': float(),
+    },
+
+    'XFX': {
+        'Current Price': float(),
+        'Current Balance': float(),
+    },
+
+    'CGN': {
+        'Current Price': float(),
+        'Current Balance': float(),
+    },
+
+    'SPARE': {
+        'Current Price': float(),
+        'Current Balance': float(),
+    },
+
+    'AR': {
+        'Current Price': float(),
+        'Current Balance': float(),
+    }
+}
+
+api_call_list = []
+def farmr_api_call():
+
+    x = requests.get('https://farmr.net/read.php?user=95534481070362624').text
+    blocks = x.split(';;')
+    for block in blocks:
+        try:
+            json_dict = json.loads(block)
+            api_call_list.append(json_dict[0])
+            crypto_name = json_dict[0]['crypto'].upper()
+            storage[crypto_name]['Current Balance'] = json_dict[0]['balance']
+        except:
+            pass
+
+
+
+
+def make_dfs():
+    for token in storage.keys():
+        if not os.path.exists('Csvs/' + str(token) + '_data.csv'):
+            df = pd.DataFrame(columns=['Balance', 'Price'])
+            df.to_csv('Csvs/'+str(token) + '_data.csv', index=False)
+
+
+def get_prices():
+    cmc_api_key = 'a98e9495-0d8f-438b-8691-cb08d162cd59'
+    cmc = coinmarketcapapi.CoinMarketCapAPI(cmc_api_key)
+    for symbol in storage.keys():
+        try:
+            data = cmc.cryptocurrency_quotes_latest(symbol=symbol, convert='USD').data
+            storage[symbol]['Current Price'] = data[symbol]['quote']['USD']['price']
+        except:
+            storage[symbol]['Current Price'] = 0
+
+
+def update_csv(input_csv, symbol, storage_dict=storage):
+    df = pd.read_csv(input_csv, header=0, index_col=0)
+    if len(df.columns) < 2:
+        df = pd.read_csv(input_csv, header=0)
+    df_cols = list(df.columns)
+    today = pd.to_datetime('now', exact=False).strftime("%d/%m/%Y %I:%M:%S")
+    new_row = pd.DataFrame(data=[[storage_dict[symbol]['Current Balance'], storage_dict[symbol]['Current Price']]],
+                           columns=df_cols, index=[today])
+    output_df = pd.concat([df, new_row], ignore_index=False).reset_index()
+    output_df.columns = ['Date', 'Balance', 'Price']
+    output_df.to_csv(input_csv, index=False)
+    # storage_dict[symbol]['Balance History'] = dict(zip(output_df['Date'], output_df['Balance']))
+
+
+def get_arweave_data():
+    url = 'https://viewblock.io/arweave/address/RzfJuyW51BmAVet9imfhcKBFDMskJcSlNXdPHH9sWHE'
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, features="lxml")
+    x = soup.find('div', {'class': "ud6aj3-3 gHxoUx"}).findNext().findNext().text
+    balance = float(x.split()[0])
+    symbol = x.split()[1]
+    storage[symbol]['Current Balance'] = balance
+
+    update_csv('Csvs/AR_data.csv', symbol)
+
+
+def get_flax_data():
+    flax_url = 'https://flaxexplorer.org/blockchain/address/xfx1jsrene5l6kvmkazpgcynz9x38sfgk2w965re9f6g0rla3ahyptkqpsppsn'
+    request = requests.get(flax_url)
+    soup = BeautifulSoup(request.text, features='lxml')
+    flax_balance = float(soup.find('span', {'class': 'stats-value'}).text.split()[0])
+
+    driver = webdriver.Chrome('./chromedriver', options=options)
+    driver.get('https://flax-og.foxypool.io/my-farmer')
+    driver.minimize_window()
+    pool_key_input = driver.find_element_by_xpath('/html/body/app-root/div/app-my-farmer/div/div/div/input')
+    pool_key_input.send_keys(
+        '8cae91ac66fd08959589339242980a88336bbb201c75a38cda84e32f1e4e1db06cbb52e2c7c57242528acab832ace197')
+    login_button = driver.find_element_by_xpath('/html/body/app-root/div/app-my-farmer/div/div/div/button')
+    login_button.click()
+    time.sleep(2)
+    flax_pool_balance = driver.find_element_by_xpath(
+        '/html/body/app-root/div/app-my-farmer/div/div/div/div[1]/div[3]/div[1]/div/div[2]/span')
+    pool_balance = float(flax_pool_balance.text.split()[0])
+    symbol = flax_pool_balance.text.split()[1]
+    driver.quit()
+
+    storage[symbol]['Current Balance'] = flax_balance + pool_balance
+
+    update_csv('Csvs/XFX_data.csv', symbol)
+
+
+def get_chia_data():
+
+    driver = webdriver.Chrome('./chromedriver', options=options)
+    driver.get('https://chia-og.foxypool.io/my-farmer')
+    driver.minimize_window()
+    pool_key_input = driver.find_element_by_xpath('/html/body/app-root/div/app-my-farmer/div/div/div/input')
+    pool_key_input.send_keys(
+        '8cae91ac66fd08959589339242980a88336bbb201c75a38cda84e32f1e4e1db06cbb52e2c7c57242528acab832ace197')
+    login_button = driver.find_element_by_xpath('/html/body/app-root/div/app-my-farmer/div/div/div/button')
+    login_button.click()
+    time.sleep(0.5)
+    chia_pool_balance = driver.find_element_by_xpath(
+        '/html/body/app-root/div/app-my-farmer/div/div/div/div[1]/div[3]/div[1]/div/div[2]/span')
+    pool_balance = float(chia_pool_balance.text.split()[0])
+    symbol = chia_pool_balance.text.split()[1]
+
+    driver.get(
+        'https://www.chiaexplorer.com/blockchain/address/xch1zxnqwv585mua5u98dhhgadxft7u9lys78vuluwtmmy273r984t9qycrtp6')
+    time.sleep(0.5)
+    balance = driver.find_element_by_xpath(
+        '//*[@id="root"]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[2]/div[3]/div[2]/span[1]').text
+    chia_balance = float(balance.split()[0])
+    symbol = balance.split()[1]
+    driver.quit()
+
+    storage[symbol]['Current Balance'] = chia_balance + pool_balance
+
+    update_csv('Csvs/XCH_data.csv', symbol)
+
+def get_spare_data():
+    update_csv('Csvs/Spare_data.csv', 'SPARE')
+
+def get_chaingreen_data():
+    update_csv('Csvs/CGN_data.csv', 'CGN')
+def execute():
+    farmr_api_call()
+    make_dfs()
+    get_prices()
+    get_arweave_data()
+    get_flax_data()
+    get_chia_data()
+    get_chaingreen_data()
+    get_spare_data()
+    return storage
+
+
+if __name__ == "__main__":
+    execute()
+    with open("output.json", "w") as outfile:
+        json.dump(storage, outfile, indent=4)
