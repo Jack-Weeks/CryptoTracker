a001dec5b13a4678146ce255799a546d7e1212e9
Jack <jacksambournweeks@gmail.com>
2021-07-04 01:08:42 +0100

    lol

--- analysis.py
+++ analysis.py
@@ -6,51 +6,61 @@ def analysis():
     with open('output.json', "r") as file:
         data = json.load(file)
     for token in data.keys():
-        USD_GBP = yf.download('GBPUSD=X', period='1d', interval='1h')
-        exchange = USD_GBP['Close'][-1]
-        df = pd.read_csv(token + '_data.csv')
-        df['Date'] = pd.to_datetime(df['Date'])
-        df['% Change in Balance'] = (df.Balance.pct_change(periods=12)) * 100
-        df['Balance Diff'] = df.Balance.diff()
-        df['Value $ '] = df['Balance'] * data[token]['Current Price']
-        df['Value £ '] = df['Value $ '] * exchange
-        df['Change in Value'] = df['Balance Diff'] * df['Price']
+        try:
+            USD_GBP = yf.download('GBPUSD=X', period='1d', interval='1h')
+            exchange = USD_GBP['Close'][-1]
+            df = pd.read_csv(token + '_data.csv')
+            df['Date'] = pd.to_datetime(df['Date'])
+            df['% Change in Balance'] = (df.Balance.pct_change(periods=12)) * 100
+            df['Balance Diff'] = df.Balance.diff()
+            df['Value $ '] = df['Balance'] * data[token]['Current Price']
+            df['Value £ '] = df['Value $ '] * exchange
+            df['Change in Value'] = df['Balance Diff'] * df['Price']
 
 
-        x = df.groupby(pd.Grouper(key = 'Date', freq='1h'))['Balance Diff'].mean()
-        y = df.groupby(pd.Grouper(key = 'Date', freq='1D'))['Balance Diff'].mean()
-        z = df.groupby(pd.Grouper(key = 'Date', freq='1W'))['Balance Diff'].mean()
-        p = df.groupby(pd.Grouper(key = 'Date', freq='1M'))['Balance Diff'].mean()
+            x = df.groupby(pd.Grouper(key = 'Date', freq='1h'))['Balance Diff'].mean()
+            y = df.groupby(pd.Grouper(key = 'Date', freq='1D'))['Balance Diff'].mean()
+            z = df.groupby(pd.Grouper(key = 'Date', freq='1W'))['Balance Diff'].mean()
+            p = df.groupby(pd.Grouper(key = 'Date', freq='1M'))['Balance Diff'].mean()
 
-        average_hourly_difference = round(x.mean(), 5)
-        average_hourly_dollar_gainz = round(average_hourly_difference * data[token]['Current Price'], 2)
+            average_hourly_difference = round(x.mean(), 5)
+            average_hourly_dollar_gainz = round(average_hourly_difference * data[token]['Current Price'], 2)
 
-        average_daily_difference = round(y.mean(), 5)
-        average_daily_dollar_gainz = round(average_daily_difference * data[token]['Current Price'], 2)
+            average_daily_difference = round(y.mean(), 5)
+            average_daily_dollar_gainz = round(average_daily_difference * data[token]['Current Price'], 2)
 
-        average_weekly_difference = round(z.mean(), 5)
-        average_weekly_dollar_gainz = round(average_weekly_difference * data[token]['Current Price'], 2)
+            average_weekly_difference = round(z.mean(), 5)
+            average_weekly_dollar_gainz = round(average_weekly_difference * data[token]['Current Price'], 2)
 
-        average_monthly_difference = round(p.mean(), 5)
-        average_monthly_dollar_gainz = round(average_monthly_difference * data[token]['Current Price'], 2)
+            average_monthly_difference = round(p.mean(), 5)
+            average_monthly_dollar_gainz = round(average_monthly_difference * data[token]['Current Price'], 2)
 
-        data[token]['Average_Hourly_Increase'] = average_hourly_difference
-        data[token]['Average_Hourly_Value_Increase'] = average_hourly_dollar_gainz
+            data[token]['Average Hourly Increase'] = average_hourly_difference
+            data[token]['AverageHourly Value Increase'] = average_hourly_dollar_gainz
 
-        data[token]['Average_Daily_Increase'] = average_daily_difference
-        data[token]['Average_Daily_Value_Increase'] = average_daily_dollar_gainz
+            data[token]['Average Daily Increase'] = average_daily_difference
+            data[token]['Average Daily Value Increase'] = average_daily_dollar_gainz
 
-        data[token]['Average_Weekly_Increase'] = average_weekly_difference
-        data[token]['Average_Weekly_Value_Increase'] = average_weekly_dollar_gainz
+            # data[token]['Average_Weekly_Increase'] = average_weekly_difference
+            # data[token]['Average_Weekly_Value_Increase'] = average_weekly_dollar_gainz
+            #
+            # data[token]['Average_Monthly_Increase'] = average_monthly_difference
+            # data[token]['Average_Monthly_Value_Increase'] = average_monthly_dollar_gainz
 
-        data[token]['Average_Monthly_Increase'] = average_monthly_difference
-        data[token]['Average_Monthly_Value_Increase'] = average_monthly_dollar_gainz
-
-        df.to_excel(token + '_analysis.xls', index=False)
-
+            df.to_excel(token + '_analysis.xls', index=False)
+        except:
+            pass
 
 
-
+    total = 0
+    for i in data.keys():
+        try:
+            price = data[i]['Current Price']
+            quantity = data[i]['Current Balance']
+            total += price * quantity
+        except:
+            pass
+    data['Totals'] = {'Total': total}
     with open("output.json", "w") as outfile:
         json.dump(data, outfile, indent=4)
     return data
--- main.py
+++ main.py
@@ -40,7 +40,9 @@ storage = {
     'AR': {
         'Current Price': float(),
         'Current Balance': float(),
-    }
+    },
+    'Totals': {
+}
 }
 
 api_call_list = []
@@ -209,17 +211,13 @@ def main():
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script></header>""")
-        file.write('<body>')
-        file.write('<table class="table table-striped table-dark"">')
+        file.write('<body class="container" style="padding: 5%">')
+        file.write('<table class="table table-striped table-bordered table-dark col-lg-4" style="width: 80%; left: 10%;right: 10%">')
         for item in data.keys():
-            # print(item)
             file.write(
                 f'<tr><td rowspan={sum(num_items(data[item])) + 1} colspan=2 style="text-align:center" class="text-center align-middle">{item}</td>')
             for i in data[item].keys():
-                # print(i)
-                # print(data[item][i])
-                file.write(f'<tr><td style="padding:10px">{i}</td><td style="padding:10px">{data[item][i]}</td></tr>')
-
+                file.write(f'<tr><td style="padding:10px">{i}</td><td style="padding:10px;text-align: right">{data[item][i]}</td></tr>')
             file.write('</tr>')
         file.write('</table>')
         file.write('</body></html)')
