fd92103acafbd57ff999c1ac5517338c2fa7d4e3
Jack <jacksambournweeks@gmail.com>
2021-07-06 19:06:32 +0100

    lol

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ def get_flax_data():
     storage[symbol]['Current Balance'] = round(flax_balance + pool_balance + pending_balance, 5)
     storage[symbol]['Wallet Balance'] = round(flax_balance, 5)
     storage[symbol]['Collateral Balance'] = round(pool_balance, 5)
-    storage[symbol]['EC'] = EC.split()[:1]
+    storage[symbol]['EC'] = EC[:9]
     storage[symbol]['Pending Balance'] = round(pending_balance,5)
 
     update_csv(plotting_path + 'XFX_data.csv', symbol)
@@ -204,7 +204,7 @@ def get_chia_data():
     storage[symbol]['Current Balance'] = round(chia_balance + pool_balance + pending_balance, 5)
     storage[symbol]['Wallet Balance'] = round(chia_balance, 5)
     storage[symbol]['Collateral Balance'] = round(pool_balance, 5)
-    storage[symbol]['EC'] = EC.split()[:1]
+    storage[symbol]['EC'] = EC[:9]
     storage[symbol]['Pending Balance'] = round(pending_balance, 5)
 
     update_csv(plotting_path + 'XCH_data.csv', symbol)
