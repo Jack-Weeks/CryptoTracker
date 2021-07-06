7c8b69506d3bd8c89d8cb234bb42673ec4efe0c4
Jack <jacksambournweeks@gmail.com>
2021-07-06 20:10:39 +0100

    lol

--- scratch_analysis.py
+++ scratch_analysis.py
@@ -148,8 +148,47 @@ new_df.to_csv('daily_gains.csv',index = False)
 # time.sleep(1.5)
 # EC = driver.find_element_by_xpath('/html/body/app-root/div/app-my-farmer/div/div/div/div[1]/div[1]/div[3]/div/div[2]')
 
-
+import yfinance as yf
 df = pd.read_csv('docs/graphing/data/XCH_data.csv')
 df['Date'] = pd.to_datetime(df['Date'])
 df['Balance_Diff'] = df['Balance'].diff()
 grouped = df.groupby(pd.Grouper(key='Date', freq='d'))['Balance_Diff'].sum()
+plotting_path = 'docs/graphing/data/'
+with open('output.json', "r") as file:
+    data = json.load(file)
+for token in data.keys():
+    USD_GBP = yf.download('GBPUSD=X', period='1d', interval='1h')
+    exchange = USD_GBP['Close'][-1]
+    df = pd.read_csv(plotting_path + token + '_data.csv')
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['% Change_in_Balance'] = (df.Balance.pct_change(periods=12)) * 100
+    df['Balance_Diff'] = df.Balance.diff()
+    df['Value_Dollars'] = df['Balance'] * data[token]['Current Price']
+    df['Value_Pounds'] = df['Value_Dollars'] / exchange
+    df['Change_in_Value'] = df['Balance_Diff'] * df['Price']
+
+    x = df.groupby(pd.Grouper(key='Date', freq='1h'))['Balance_Diff'].sum()
+    y = df.groupby(pd.Grouper(key='Date', freq='1D'))['Balance_Diff'].sum()
+    z = df.groupby(pd.Grouper(key='Date', freq='1W'))['Balance_Diff'].sum()
+    p = df.groupby(pd.Grouper(key='Date', freq='1M'))['Balance_Diff'].sum()
+    grouped_daily = df.groupby(pd.Grouper(key='Date', freq='1D'))['Balance'].mean().to_frame()
+
+    average_hourly_difference = round(x.mean(), 5)
+    average_hourly_dollar_gainz = round(average_hourly_difference * data[token]['Current Price'], 2)
+
+    diff = (grouped_daily['Balance'].pct_change().values[-1] * 100)
+    average_daily_difference = round(y.mean(), 5)
+    average_daily_dollar_gainz = round(average_daily_difference * data[token]['Current Price'], 2)
+
+    average_weekly_difference = round(z.mean(), 5)
+    average_weekly_dollar_gainz = round(average_weekly_difference * data[token]['Current Price'], 2)
+
+    average_monthly_difference = round(p.mean(), 5)
+    average_monthly_dollar_gainz = round(average_monthly_difference * data[token]['Current Price'], 2)
+
+    # data[token]['Average Hourly Increase'] = average_hourly_difference
+    # data[token]['Average Hourly Value Increase'] = average_hourly_dollar_gainz
+    data[token]['Current Value'] = '$' + str(round(df.iloc[-1:]['Value_Dollars'].values[0], 2))
+    data[token]['Average Daily Increase'] = str(average_daily_difference) + ' ' + token
+    data[token]['Average Daily Value Increase'] = '$' + str(average_daily_dollar_gainz)
+    data[token]['Daily % Change'] = str(round(diff, 2)) + '%'
