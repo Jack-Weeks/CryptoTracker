aabdf740c03f4a9bd15c50530927ce9f0e6a774f
Jack <jacksambournweeks@gmail.com>
2021-07-31 21:35:27 +0100

    lol

--- Posatio.py
+++ Posatio.py
@@ -6,6 +6,10 @@ url = 'https://market.posat.io/'
 request = requests.get(url)
 soup = BeautifulSoup(request.text, parser='xml')
 cards = soup.findAll('div', {'class': 'card'})
+
+#### Load Json with Previous Deets
+
+
 for card in cards:
     coin = card.findNext('div').text.strip()
     price = card.findNext('div').findNext('div').text.strip()
